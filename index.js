72eba56885d0dac7825f1a219b48fbfcc7279df8
basarancr <h.shiota@basaran.co.jp>
2023-05-24 21:24:42 +0900

    second commit

--- index.js
+++ index.js
@@ -10,9 +10,8 @@ document.querySelectorAll('.true').forEach((item, index) => {
 document.querySelectorAll('.false').forEach((item, index) => {
     item.addEventListener('click', function() {
         let trueTextElements = document.querySelectorAll('.falsetext');
-        trueTextElements[index].textContent = 'Oh No!';
+        trueTextElements[index].textContent = 'Boooo!';
         trueTextElements[index].classList.add('perfectStyle2');
     });
 });
-
 
